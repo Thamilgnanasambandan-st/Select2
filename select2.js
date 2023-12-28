bab7749beb22b62127ea6b80cdb7e6fc30433448
Thamilgnanasambandan Thangavel <>
2023-12-28 18:31:00 +0530

    add class

--- select2.js
+++ select2.js
@@ -16,6 +16,7 @@
             init: function () {
                 $el = $jq
                 $jq.addClass(`drop2-select`)
+                $jq.addClass(`drop2-initiated`)
                 // Create drop conatiner and header
                 $jq.after(`<div class='drop-container ${$jq.attr('multiple') ? 'multiselect-drop' : ''} '><div class='drop-header ${settings.customeheader ? 'drop-custom-header' : ''}'>${settings.customeheader ? settings.customeheader : 'Select Options'}</div> <div class='drop-body' drop-render='hide'><div class='drop-drawer'><ul></ul>${$jq.attr('multiple') ? "<div class='drop-action-btn'><a class='drop-cancel'>Cancel</a><a class='drop-select'>submit</a></div>" : ''}</div>${ settings.selectedDrawer && isMultiple ? '<div class="selected-options-container"><div class="selected-options"></div></div>' : ''}</div>`)
                 component()
